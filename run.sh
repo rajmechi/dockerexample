4b3a981ced5e3089d5a813a66f6ca9dc1d5b8006
rajmechi <39564006+rajmechi@users.noreply.github.com>
2019-05-30 01:30:19 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-while true; do sleep 15 ; echo "background"; done &
+while true; do sleep 15 ; echo "hello background"; done &
 
-while true; do sleep 12 ; echo "foreground"; done
+while true; do sleep 12 ; echo "hello foreground"; done
